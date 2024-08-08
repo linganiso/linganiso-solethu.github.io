fab5fd1159ab706348d0ded1436ca816efaf4054
Solethu Linganiso <163216758+linganiso@users.noreply.github.com>
2024-08-08 22:44:44 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    btnText.style.display = "none";
+  }
+
+  function myFunc() {
+    var dots = document.getElementById("dots-skills");
+    var moreText = document.getElementById("more-skills");
+    var btnText = document.getElementById("myBtn-skills");
+  
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    btnText.style.display = "none";
+  }
+
+  function myFuncHotel() {
+    var dots = document.getElementById("dots-hotel");
+    var moreText = document.getElementById("more-hotel");
+    var btnText = document.getElementById("myBtn-hotel");
+  
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    btnText.style.display = "none";
+  }
+
+  function myFuncSchool() {
+    var dots = document.getElementById("dots-school");
+    var moreText = document.getElementById("more-school");
+    var btnText = document.getElementById("myBtn-school");
+  
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    btnText.style.display = "none";
+  }
+
+  function myFuncData() {
+    var dots = document.getElementById("dots-data");
+    var moreText = document.getElementById("more-data");
+    var btnText = document.getElementById("myBtn-data");
+  
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    btnText.style.display = "none";
+  }
